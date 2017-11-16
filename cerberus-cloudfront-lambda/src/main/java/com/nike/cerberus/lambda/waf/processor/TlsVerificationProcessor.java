3849b1faedd35c793967867a837372e0b917961b
Todd Lisonbee <tlisonbee@users.noreply.github.com>
2017-11-15 20:41:59 -0800

    Adding env variable to ignore certain paths. (#13)

--- cerberus-cloudfront-lambda/src/main/java/com/nike/cerberus/lambda/waf/processor/TlsVerificationProcessor.java
+++ cerberus-cloudfront-lambda/src/main/java/com/nike/cerberus/lambda/waf/processor/TlsVerificationProcessor.java
@@ -1,6 +1,6 @@
 package com.nike.cerberus.lambda.waf.processor;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
 import com.nike.cerberus.lambda.waf.CloudFrontLogEvent;
 import com.nike.cerberus.lambda.waf.CloudFrontLogHandlerConfig;
 import org.apache.commons.lang3.StringUtils;
@@ -8,6 +8,10 @@ import org.apache.log4j.Logger;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+
+import static org.apache.commons.lang3.StringUtils.split;
+import static org.apache.commons.lang3.StringUtils.trimToEmpty;
 
 /**
  * For tracking down clients that are not using TLS1.2
@@ -15,9 +19,17 @@ import java.util.List;
 public class TlsVerificationProcessor implements Processor {
 
     private static final String TLS_1_2 = "TLSv1.2";
-    private static final List<String> ACCEPTABLE_TLS_VERSIONS = ImmutableList.of(
-            TLS_1_2
+    private static final String NON_TLS = "-";
+    private static final Set<String> ACCEPTABLE_TLS_VERSIONS = Sets.newHashSet(
+            TLS_1_2,
+            // non-TLS isn't really acceptable but it can be ignored since it can never reach the system
+            NON_TLS
     );
+
+    /**
+     * List of paths to ignore
+     */
+    private static final Set<String> SUPPRESSED_PATHS = Sets.newHashSet(split(trimToEmpty(System.getenv("TLS_VERIFICATION_SUPPRESSED_PATHS")), ","));
 
     private final Logger log = Logger.getLogger(getClass());
 
@@ -25,7 +37,8 @@ public class TlsVerificationProcessor implements Processor {
     public void processLogEvents(List<CloudFrontLogEvent> events, CloudFrontLogHandlerConfig config, String bucketName) {
         List<CloudFrontLogEvent> nonAcceptableEvents = new LinkedList<>();
         events.forEach(event -> {
-            if ( ! ACCEPTABLE_TLS_VERSIONS.contains(event.getSslProtocol()) ) {
+            if (!ACCEPTABLE_TLS_VERSIONS.contains(event.getSslProtocol())
+                    && !SUPPRESSED_PATHS.contains(event.getCsUriStem())) {
                 nonAcceptableEvents.add(event);
             }
         });
@@ -37,6 +50,7 @@ public class TlsVerificationProcessor implements Processor {
 
         StringBuilder sb = new StringBuilder("Cloud Front Log Event Handler - TLS Verification Processor run summary");
         sb.append('\n').append("Running Environment: ").append(config.getEnv()).append('\n');
+        sb.append('\n').append("Ignoring Paths: " + SUPPRESSED_PATHS).append('\n');
         nonAcceptableEvents.forEach(event -> {
             sb
                     .append("TLS Version: ").append(event.getSslProtocol())
